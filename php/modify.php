7e610324372610ea17fd0b39734881b2b52350e5
Patryk Goszczko <goszczkopatryk@gmail.com>
2020-12-22 14:59:31 +0100

    Added query for removing notes

--- php/modify.php
+++ php/modify.php
@@ -31,11 +31,20 @@ try
 		$query = 'INSERT INTO notes (date, description) VALUE (:date, :desc)';	
 	}
 
+	// Query for removing
+	if ($isID && !$isDesc && !$isDate)
+	{
+		$query = 'DELETE FROM notes WHERE id=:id';
+	}
+	
+
 	$q = $db_conn->prepare($query); 
 	if ($isDesc) $q->bindParam(':desc', $_POST['desc']);
 	if ($isDate) $q->bindParam(':date', $_POST['date']);
 	if ($isID)	 $q->bindParam(':id', $_POST['id']);
 	$q->execute();
+
+	
 }
 catch(PDOException $e)
 {
